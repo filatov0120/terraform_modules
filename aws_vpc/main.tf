5323e798e45c6ee92650fffc23aab22e99e586c5
filicipa <filicipa@gmail.com>
2023-03-20 23:59:32 +0200

    Fix dns and public IP

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,25 +1,28 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_vpc
-  tags = merge(var.common_tags, { Name = "${var.proj_name}-VPC" })
+  cidr_block           = var.cidr_vpc
+  enable_dns_hostnames = true
+  tags                 = merge(var.common_tags, { Name = "${var.proj_name}-VPC" })
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr
-  availability_zone = var.azs
-  tags = merge(var.common_tags, { Name = "${var.proj_name}_public_subnet" })
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.azs
+  tags                    = merge(var.common_tags, { Name = "${var.proj_name}_public_subnet" })
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidr
   availability_zone = var.azs
-  tags = merge(var.common_tags, { Name = "${var.proj_name}_private_subnet" })
+  tags              = merge(var.common_tags, { Name = "${var.proj_name}_private_subnet" })
+
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-  tags = merge(var.common_tags, { Name = "${var.proj_name}_vpc_igw" })
+  tags   = merge(var.common_tags, { Name = "${var.proj_name}_vpc_igw" })
 }
 
 resource "aws_route_table" "igw_rt" {
@@ -28,6 +31,7 @@ resource "aws_route_table" "igw_rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+
   tags = merge(var.common_tags, { Name = "RT_to_igw_for_${var.proj_name}" })
 }
 
