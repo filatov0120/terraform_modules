cf4bc84993c0c7dc3470e6a2d0408373a5cd9761
filicipa <filicipa@gmail.com>
2023-03-25 14:53:52 +0200

    Test without custom RT

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -25,17 +25,17 @@ resource "aws_internet_gateway" "gw" {
   tags   = merge(var.vpc_tags, { Name = "${var.proj_name}_vpc_igw" })
 }
 
-resource "aws_route_table" "igw_rt" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+# resource "aws_route_table" "igw_rt" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
 
-  tags = merge(var.vpc_tags, { Name = "RT_to_igw_for_${var.proj_name}" })
-}
+#   tags = merge(var.vpc_tags, { Name = "RT_to_igw_for_${var.proj_name}" })
+# }
 
-resource "aws_route_table_association" "subnet_asso" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.igw_rt.id
-}
+# resource "aws_route_table_association" "subnet_asso" {
+#   subnet_id      = aws_subnet.private_subnet.id
+#   route_table_id = aws_route_table.igw_rt.id
+# }
