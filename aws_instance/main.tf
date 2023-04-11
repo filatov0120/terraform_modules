18c62820eab19c0e4def2a02aac15bc27c8b40f7
filicipa <filicipa@gmail.com>
2023-04-11 22:04:12 +0300

    Replace vpc_security_group_ids

--- aws_instance/main.tf
+++ aws_instance/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "this" {
-  instance_type     = var.instance_type
-  ami               = var.ami
-  availability_zone = var.azs
-  security_groups   = [aws_security_group.this.id]
-  subnet_id         = var.subnet_id
-  key_name          = var.ssh_key
+  instance_type          = var.instance_type
+  ami                    = var.ami
+  availability_zone      = var.azs
+  vpc_security_group_ids = [aws_security_group.this.id]
+  subnet_id              = var.subnet_id
+  key_name               = var.ssh_key
 
   root_block_device {
     volume_size = var.root_block_size
