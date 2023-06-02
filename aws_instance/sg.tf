4aa2c62b0b3610662b2a6f50c95ff6f3b941ad67
filicipa <filicipa@gmail.com>
2023-06-02 22:23:05 +0300

    added udp and tcp ports

--- aws_instance/sg.tf
+++ aws_instance/sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "this" {
 resource "aws_security_group_rule" "access_tcp_from_internet" {
   security_group_id = aws_security_group.this.id
   type              = "ingress"
-  count             = length(var.allow_ports)
+  count             = length(var.allow_tcp_ports)
   from_port         = element(var.allow_tcp_ports, count.index)
   to_port           = element(var.allow_tcp_ports, count.index)
   description       = "Allow from internet to tcp port ${element(var.allow_tcp_ports, count.index)}"
@@ -18,7 +18,7 @@ resource "aws_security_group_rule" "access_tcp_from_internet" {
 resource "aws_security_group_rule" "access_udp_from_internet" {
   security_group_id = aws_security_group.this.id
   type              = "ingress"
-  count             = length(var.allow_ports)
+  count             = length(var.allow_udp_ports)
   from_port         = element(var.allow_udp_ports, count.index)
   to_port           = element(var.allow_udp_ports, count.index)
   description       = "Allow from internet to udp port ${element(var.allow_udp_ports, count.index)}"
