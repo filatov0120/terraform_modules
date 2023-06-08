496f4a56d3c0d06aae199863f2ccd1bcd667b48c
Alex <alexander.f@blaize.tech>
2023-06-08 16:41:30 +0300

    change protocol rules

--- aws_instance_without_eip/sg.tf
+++ aws_instance_without_eip/sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group_rule" "access_tcp_from_internet" {
   count             = length(var.allow_ports)
   from_port         = element(var.allow_tcp_ports, count.index)
   to_port           = element(var.allow_tcp_ports, count.index)
-  description       = "Allow from internet to tcp port ${element(var.allow_ports, count.index)}"
+  description       = "Allow from internet to tcp port ${element(var.allow_tcp_ports, count.index)}"
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
@@ -21,8 +21,8 @@ resource "aws_security_group_rule" "access_udp_from_internet" {
   count             = length(var.allow_ports)
   from_port         = element(var.allow_udp_ports, count.index)
   to_port           = element(var.allow_udp_ports, count.index)
-  description       = "Allow from internet to udp port ${element(var.allow_ports, count.index)}"
-  protocol          = "tcp"
+  description       = "Allow from internet to udp port ${element(var.allow_udp_ports, count.index)}"
+  protocol          = "udp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
